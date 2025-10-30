fcb542b7127b88af2c86ec3c3d71697bf7dae758
Michael Huynh <mhdrakemoons@gmail.com>
2025-10-30 14:53:11 +0200

    lots of change1s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -37,7 +38,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <body className={`${plusJakarta.variable} font-display antialiased bg-background-light text-[#111827]`}>
-        <ScrollToTop />
+        <Suspense fallback={null}>
+          <ScrollToTop />
+        </Suspense>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
